de09f0445239934128aff949150549aa5b1d91aa
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-08-16 22:40:09 +0900

    baekjoon 2798

---
+++ Algorithm/src/baekjoon/Main_2798.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_2798 {
+
+	public static void main(String[] args)throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int card = sc.nextInt();
+		int limit = sc.nextInt();
+		
+		int[]arr =new int[card];
+		
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int max=0;
+		for(int i=0;i<arr.length-2;i++) {
+			for(int j=i+1;j<arr.length-1;j++) {
+				for(int k=j+1;k<arr.length;k++) {
+					if(max<arr[i]+arr[j]+arr[k]&&arr[i]+arr[j]+arr[k]<=limit) max=arr[i]+arr[j]+arr[k];
+				}
+			}
+		}
+		System.out.println(max);
+		sc.close();
+	}
+}
