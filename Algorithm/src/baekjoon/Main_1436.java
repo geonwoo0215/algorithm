64df676333a0b6b0d20286516ccddaf43a630d75
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-08-17 00:06:16 +0900

    baekjoon 1436

---
+++ Algorithm/src/baekjoon/Main_1436.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_1436 {
+
+	public static void main(String[] args)throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int cnt=0;
+		int num=665;
+	
+		
+		while(cnt!=n) {
+			int temp = ++num;
+			while(temp>0) {
+				if(temp%1000==666) {
+					cnt++;
+					break;
+				}
+				else {
+					temp/=10;
+				}
+			}
+		}
+		sc.close();
+		System.out.println(num);
+	}
+}
