9dc117bc320b9649c5b4e65a57951fc52cb3f432
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-08-26 20:18:57 +0900

    baekjoon 15649

---
+++ Algorithm/src/baekjoon/Main_15649.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+
+
+public class Main_15649 {
+
+	static int N;
+	static int M;
+	
+	static boolean[] visited;
+	static int[] map;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		map = new int[M];
+		visited= new boolean[N];
+		dfs(0);
+		sc.close();
+		
+	}
+	
+	static void dfs(int depth) {
+		
+		if(depth==M) {
+			for(int i=0;i<map.length;i++) {
+				System.out.print(map[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=0;i<N;i++) {
+			if(visited[i]==false) {
+				visited[i]=true;
+				map[depth]=i+1;
+				dfs(depth+1);
+				visited[i]=false;
+			}
+	
+		}
+	}
+}
