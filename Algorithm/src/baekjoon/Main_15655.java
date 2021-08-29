cc860a7fda6d70b0bed58543ef1083d01e4c97f6
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-08-30 05:08:55 +0900

    baekjoon 15655

---
+++ Algorithm/src/baekjoon/Main_15655.java
@@ -0,0 +1,55 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_15655 {
+
+	static int N;
+	static int M;
+	static int arr[];
+	static int ans[];
+	static boolean visited[];
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		arr = new int[N];
+		ans = new int[M];
+		visited = new boolean[N];
+		
+		for(int i=0;i<N;i++) {
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		dfs(0,0);
+		sc.close();
+			
+	}
+	
+	static void dfs(int start,int depth) {
+		
+		if(depth ==M) {
+			for(int i=0;i<ans.length;i++) {
+				System.out.print(ans[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=start;i<N;i++) {
+			if(visited[i]==false) {
+				visited[i]=true;
+				ans[depth]=arr[i];
+				dfs(i,depth+1);
+				visited[i]=false;
+			}
+		}	
+	}
+}
