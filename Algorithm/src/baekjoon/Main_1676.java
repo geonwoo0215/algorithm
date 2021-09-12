d74162bf87535f27a7eca7e93e949ab06b2773ce
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-09-13 03:34:06 +0900

    baekjoon 1676

--- Algorithm/src/baekjoon/Main_1676.java
+++ Algorithm/src/baekjoon/Main_1676.java
@@ -9,23 +9,42 @@ public class Main_1676 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int count = 0;
 		
+		StringBuilder sb = new StringBuilder();
 		
-		for(int i=1;i<=N;i++) {
-			int temp=i;
-			if(temp%5==0) {
-				while(temp%5==0) {
-					count++;
-					temp/=5;
+		int T = sc.nextInt();
+		
+		for(int i=0;i<T;i++) {
+			int t = sc.nextInt();
+			int[] arr = new int[t];
+			for(int x=0;x<t;x++) {
+				arr[x]=sc.nextInt();
+			}
+			long sum =0;
+			for(int j=0;j<t;j++) {
+				int temp = arr[j];
+				for(int k=j+1;k<t;k++) {
+					sum+=Aratos(temp,arr[k]);
 				}
 			}
+			sb.append(sum+"\n");
 		}
+		System.out.print(sb);
 		
-		System.out.print(count);
 		sc.close();
-	
+		
 	}
+	
+	static int Aratos(int a,int b) {
+		int max=Math.max(a, b);
+		int min=Math.min(a, b);
+		
+		if(max%min==0) return min;
+		else return Aratos(min,max%min);
+	}
+	
 }
+	
+
+
 
