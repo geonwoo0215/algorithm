f9ef9ef5f1abf1cbc713ca3dc8b8cf18f66b726a
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-09-12 17:06:38 +0900

    baekjoon 10799

---
+++ Algorithm/src/baekjoon/Main_10799.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Main_10799{
+
+	public static void main(String[] arg) {
+	    
+		
+		Stack<Character> st = new Stack<>();
+		
+		char last=' ';
+		
+		int count = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		String batch = sc.next();
+
+		for(int i=0;i<batch.length();i++) {
+			if(batch.charAt(i)=='(') {
+				st.add(batch.charAt(i));
+				last = '(';
+			}
+			else {
+				st.pop();
+				if(last=='(') {
+					count+=st.size();
+				}
+				else {
+					count+=1;
+				}
+				last=')';
+			}
+		}
+		System.out.println(count);
+		sc.close();		
+	}
+	
+}
