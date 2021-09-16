e5aeef9add3f725725546c399538273b7b3ed6a1
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-09-16 21:01:44 +0900

    baekjoon 10808

---
+++ Algorithm/src/baekjoon/Main_10808.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class Main_10808 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String str = bf.readLine();
+		
+		
+		int[] Alphabet = new int[26];
+		
+		for(int i=0;i<str.length();i++) {
+			
+			Alphabet[str.charAt(i)-'a']++;
+		}
+		
+		for(int i=0;i<Alphabet.length;i++) {
+			System.out.printf("%d ",Alphabet[i]);
+		}
+	}
+}
+
