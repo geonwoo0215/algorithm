80642d22fd27a5536fe25a2f727a29c70994085b
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-10-01 05:22:17 +0900

    baekjoon 2338

---
+++ Algorithm/src/baekjoon/Main_2338.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main_2338 {
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		System.out.println(a.add(b));
+		System.out.println(a.subtract(b));
+		System.out.println(a.multiply(b));
+	}
+}
