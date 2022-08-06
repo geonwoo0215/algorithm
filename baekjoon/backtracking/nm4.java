b64ba09fdbc050e6730d5385a2cee48d779f27e3
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-06 20:00:48 +0900

    백준>backtracking>nm4(15652)

---
+++ baekjoon/backtracking/nm4.java
@@ -0,0 +1,41 @@
+package backtracking;
+
+import java.util.Scanner;
+
+public class nm4 {
+
+    private static int[] arr;
+    private static final StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        arr = new int[m];
+
+        dfs(n, m, 0, 0);
+
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int n, int m, int depth, int start) {
+
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(n,m,depth+1,i);
+        }
+
+    }
+
+}
