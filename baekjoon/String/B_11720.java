08918c82f72b849fa2cba388242349e37cad5dfa
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-10 16:49:33 +0900

    백준>string>숫자의 합(11720)

---
+++ baekjoon/String/B_11720.java
@@ -0,0 +1,20 @@
+package String;
+
+import java.util.Scanner;
+
+public class B_11720 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        String str = scanner.next();
+        char[] ch = str.toCharArray();
+        int sum = 0;
+        for (int c : ch) {
+            sum += c - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
