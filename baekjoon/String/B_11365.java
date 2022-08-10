ba807318f016ab7d2af19f226534ae052eb1b2d4
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-10 17:09:42 +0900

    백준>string>!밀비 급일(11365)

---
+++ baekjoon/String/B_11365.java
@@ -0,0 +1,18 @@
+package String;
+
+import java.util.Scanner;
+
+public class B_11365 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+        while (!str.equals("END")) {
+            System.out.println(new StringBuffer(str).reverse());
+            str = scanner.nextLine();
+        }
+    }
+
+}
