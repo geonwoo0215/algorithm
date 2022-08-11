83bc6106bef7aa399d5c05ecfe8618fb5284d8b9
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-12 02:29:21 +0900

    백준>string>이 구역의 승자(20154)

---
+++ baekjoon/String/B_20154.java
@@ -0,0 +1,29 @@
+package String;
+
+import java.util.Scanner;
+
+public class B_20154 {
+
+    private static final int[] arr = {3, 2, 1, 2, 3, 3, 3, 3, 1, 1, 3, 1, 3, 3, 1, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 1};
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+
+        int sum = 0;
+        char[] c = str.toCharArray();
+
+        for (char value : c) {
+            sum += arr[(value - 'A')]%10;
+        }
+
+        if (sum % 2 == 0) {
+            System.out.println("You're the winner?");
+            return;
+        }
+
+        System.out.println("I'm a winner!");
+
+    }
+}
