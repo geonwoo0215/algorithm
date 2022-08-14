1347533174c0dc9c2ee19c3cfc3b82a733904ae9
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-14 18:35:07 +0900

    백준>string>듣보잡(1764)

---
+++ baekjoon/String/B_1764.java
@@ -0,0 +1,40 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class B_1764 {
+
+    public static void main(String[] args) throws IOException {
+
+        Set<String> strings = new HashSet<>();
+        List<String> ans = new LinkedList<>();
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            strings.add(bf.readLine());
+        }
+        for (int i = 0; i < m; i++) {
+            String str = bf.readLine();
+            if (strings.contains(str)) {
+                ans.add(str);
+            }
+        }
+
+        Collections.sort(ans);
+
+        System.out.println(ans.size());
+
+        for (String an : ans) {
+            System.out.println(an);
+        }
+    }
+}
