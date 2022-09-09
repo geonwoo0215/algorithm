ba6be9f1a41a205b0448e9604933d875b0bdc2d9
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-09-09 17:17:01 +0900

    프로그래머스>String>다음 큰 숫자

---
+++ programmers/next_number.java
@@ -0,0 +1,26 @@
+public class next_number {
+    public int solution(int n) {
+
+        int cnt = countOne(Integer.toString(n, 2));
+
+        while (true) {
+            int temp = countOne(Integer.toString(++n, 2));
+            if(temp==cnt){
+                break;
+            }
+        }
+        return n;
+    }
+
+    private int countOne(String s) {
+
+        int num = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) - '0' == 1) {
+                num++;
+            }
+        }
+        return num;
+    }
+}
