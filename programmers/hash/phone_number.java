3d1c9b404b305f39afb11c6215df6fc0ab54e6ec
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-09-17 01:53:37 +0900

    프로그래머스>hash>전화번호 목록

---
+++ programmers/hash/phone_number.java
@@ -0,0 +1,25 @@
+package hash;
+
+import java.util.HashMap;
+
+public class phone_number {
+
+    public boolean solution(String[] phone_book) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+
+        for (int i = 0; i < phone_book.length; i++) {
+            hashMap.put(phone_book[i], i);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (hashMap.containsKey(phone_book[i].substring(0, j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
